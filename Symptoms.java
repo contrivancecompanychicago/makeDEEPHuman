c069b4b3ba5e29ca9c3137ef910c68413d5b2ee3
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-03-19 10:40:46 -0500

    Create Symptoms.java

---
+++ Symptoms.java
@@ -0,0 +1,45 @@
+public class Symptoms extends Main {
+
+  public static void printName(String symptom) {
+
+    System.out.println("Sometimes, during the PIRDDIIQ Pretest criteria selection, patients will describe symptoms that may or may not necessarily be related to diabetes like " + symptom + ".");
+    
+  }
+
+  public static void main(String[] args) {
+
+    String symptom = "muscle spasm";
+    printName(symptom);
+
+    String symptom1 = "neck pain";
+    printName(symptom1);
+
+    String symptom2 = "excessive pain";
+    printName(symptom2);
+
+    String symptom3 = "anxiety";
+    printName(symptom3);
+
+    String symptom4 = "ear swelling";
+    printName(symptom4);
+
+    String symptom5 = "abdominal swelling";
+    printName(symptom5);
+
+    String symptom6 = "headache";
+      printName(symptom6);
+
+    String symptom7 = "diarrhea";
+    printName(symptom7);
+
+    String symptom8 = "leg pain";
+    printName(symptom8);
+
+    String symptom9 = "sneezing";
+    printName(symptom9);
+
+    String symptom10 = "confusion";
+    printName(symptom10);
+    
+  }
+}
