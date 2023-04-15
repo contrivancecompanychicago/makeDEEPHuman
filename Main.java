a5255779b1ff4e9a2684ed13f1a33e493bc02465
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-04-15 10:45:43 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 package com.imhotepbizarrostriage;
+
+import java.lang.Object
 import com.imhotepbizarrostriage.BitwiseHuman;
 import com.imhotepbizarrostriage.Symptoms;
 
-class Main {
+class Main 
+{
 }
